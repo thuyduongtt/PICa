98f8c7618b07efa12ff5c9c53c6224a74d916f60
Duong Tran <d@dttt.io>
2023-11-16 15:13:39 +0100

    ...

--- extract_features.py
+++ extract_features.py
@@ -40,6 +40,9 @@ def extract_img_feat(ds_root_dir, ds_name, split='train', limit=0):
             print(f'[{count}] {img.name}')
         if 0 < limit <= count:
             break
+
+    features_list = np(features_list)
+    print('feature_list:', features_list.shape)
     np.save(f'{ds_root_dir}/{ds_name}/{ds_name}_{split}_feats.npy', features_list)
 
 
@@ -54,10 +57,14 @@ def extract_question_feat(ds_root_dir, ds_name, split='train', limit=0):
         txt = clip.tokenize(d['question']).to(device)
         with torch.no_grad():
             txt_feat = model.encode_text(txt)
+            print(txt_feat.shape)
             features_list.append(txt_feat.cpu())
         count += 1
         if count % 100 == 0:
             print(f'[{count}]')
+
+    features_list = np(features_list)
+    print('feature_list:', features_list.shape)
     np.save(f'{ds_root_dir}/{ds_name}/{ds_name}_{split}_questions_feats.npy', features_list)
 
 
